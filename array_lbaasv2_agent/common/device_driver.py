882c2d55b473e4d299ba69c91c276f6035b7b795
jarod.w <wl.jarod@gmail.com>
2019-08-02 10:49:04 +0800

    Fix the issue that it can't update L7 configuration when update_pool
    Signed-off-by: jarod.w <wl.jarod@gmail.com>

--- array_lbaasv2_agent/common/device_driver.py
+++ array_lbaasv2_agent/common/device_driver.py
@@ -368,8 +368,9 @@ class ArrayADCDriver(object):
                 self.driver.update_health_monitor(argu)
 
             # update L7 policy
-            if obj['l7_policies']:
-                for policy in obj['l7_policies']:
+            listener = obj['listener']
+            if listener:
+                for policy in listener['l7_policies']:
                     self.create_l7policy(policy, updated=True)
                     if policy['rules']:
                         self.create_all_rules(policy)
